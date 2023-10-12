e7f1c0d8f3d00568fb410ad1999528c646e3eb83
tippuks <131861990+tippuks@users.noreply.github.com>
2023-10-12 14:51:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 
+
     var map = L.map('map').setView([17.31437299307749, 78.46847035721423], 10);
 
-    // OSM TILE LAYER
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors; Created By <a href= "https://github.com/tippuks">Tippu K S</a> '
-    // }).addTo(map);
-    
-    // CUSTOM TILE LAYER
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-        maxZoom: 20,
-        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> Created By <a href= "https://github.com/tippuks">Tippu K S</a>'
+    //OSM TILE LAYER
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors; Created By <a href= "https://github.com/tippuks">Tippu K S</a> '
     }).addTo(map);
+    
+    // // CUSTOM TILE LAYER
+    // L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+    //     maxZoom: 20,
+    //     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> Created By <a href= "https://github.com/tippuks">Tippu K S</a>'
+    // }).addTo(map);
     
     L.marker([17.31437299307749, 78.46847035721423],{
         title:'Marker',
